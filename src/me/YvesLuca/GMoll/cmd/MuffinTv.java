285908d4c13a3d50f1f073499dc00d1854af12a9
GameduJS <Sagich@dir.net>
2020-05-07 19:43:28 +0200

    Muffine CMD

---
+++ src/me/YvesLuca/GMoll/cmd/MuffinTv.java
@@ -0,0 +1,43 @@
+package me.YvesLuca.GMoll.cmd;
+
+import org.bukkit.Location;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import me.YvesLuca.GMoll.Main;
+
+public class MuffinTv implements CommandExecutor {
+
+	private Main plugin;
+	
+	public MuffinTv(Main instance) {
+		plugin = instance;
+	}
+	
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if(label.equalsIgnoreCase("Muffin-Tv11")) {
+			if(sender instanceof Player) {
+				
+				Player player = (Player) sender;
+				
+				Player Muffin_Tv11 = plugin.getServer().getPlayer("Muffin_Tv11");
+				if(Muffin_Tv11 == null || !Muffin_Tv11.isOnline()) {
+					player.sendMessage("Der Asi is not online ha ;(");
+					return(false);
+				}else {
+					Location location = Muffin_Tv11.getLocation();
+					location.setY(location.getY() + 3);
+					Muffin_Tv11.teleport(location);
+					return true;
+				}
+				
+			}
+		}
+		return false;
+	}
+
+}
