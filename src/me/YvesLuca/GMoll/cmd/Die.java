c0921e019e3148cb6a6ed367d7e7d659e9626692
GameduJS <Sagich@dir.net>
2020-05-07 23:28:27 +0200

    remove some shit

--- src/me/YvesLuca/GMoll/cmd/Die.java
+++ src/me/YvesLuca/GMoll/cmd/Die.java
@@ -38,19 +38,13 @@ public class Die implements CommandExecutor, Listener {
 				player.setHealth(0);
 				Bukkit.broadcastMessage(ChatColor.AQUA + player.getName() + " hatte keine Lust mehr in der selben Welt wie Muffin_Tv11 zu leben!");
 				
-				int xpPlayer = (int) (Experience.getExp(player) * 0.1);
-				xpPlayer = this.ensureRange(xpPlayer, 5, 20);
-				player.sendMessage("XP: " + Experience.getExp(player) + "*0.1 but contstrained: " + (xpPlayer));
-				Experience.changeExp(player, Experience.getExp(player));
+				
 			}	
 			return true;
 		}
 		return false;
 	}
 	
-	private int ensureRange(int value, int min, int max) {
-		return Math.min(Math.max(value, min), max);
-	}
 	
 	//on player death event
 	@EventHandler
