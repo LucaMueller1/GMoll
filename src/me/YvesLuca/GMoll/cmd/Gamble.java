ec79f2b5f0e4df30be4c49cb1145b6b74c05596e
REIGNjs <unconfigured@null.spigotmc.org>
2020-05-15 23:41:06 +0200

    changed music behaviour

--- src/me/YvesLuca/GMoll/cmd/Gamble.java
+++ src/me/YvesLuca/GMoll/cmd/Gamble.java
@@ -46,11 +46,11 @@ public class Gamble implements CommandExecutor, Listener {
 		casinoPlayers = new ArrayList<CasinoPlayer>();
 		
 		casinoSlots = new CasinoSlot[5];
-		casinoSlots[0] = new CasinoSlot(new ItemStack(Material.EMERALD), 0.1, 1, 2);			//5% spin chance, 100% per slot (starting at 3)
-		casinoSlots[1] = new CasinoSlot(new ItemStack(Material.DIAMOND), 0.20, 0.5, 2);		//15% spin chance, 50% per slot (starting at 3)
-		casinoSlots[2] = new CasinoSlot(new ItemStack(Material.GOLD_INGOT), 0.25, 0.2, 3);		//20% spin chance, 20% per slot (starting at 3)
-		casinoSlots[3] = new CasinoSlot(new ItemStack(Material.IRON_INGOT), 0.30, 0.05, 3);	//25% spin chance, 5% per slot (starting at 3)
-		casinoSlots[4] = new CasinoSlot(new ItemStack(Material.COAL), 0.15, 0, 3);				//35% spin chance, 0% per slot
+		casinoSlots[0] = new CasinoSlot(new ItemStack(Material.EMERALD), 0.05, 1, 2);			//5% spin chance, 100% per slot (starting at 2)
+		casinoSlots[1] = new CasinoSlot(new ItemStack(Material.DIAMOND), 0.20, 0.35, 2);		//20% spin chance, 35% per slot (starting at 2)
+		casinoSlots[2] = new CasinoSlot(new ItemStack(Material.GOLD_INGOT), 0.25, 0.2, 3);		//25% spin chance, 20% per slot (starting at 3)
+		casinoSlots[3] = new CasinoSlot(new ItemStack(Material.IRON_INGOT), 0.30, 0.05, 3);	//30% spin chance, 5% per slot (starting at 3)
+		casinoSlots[4] = new CasinoSlot(new ItemStack(Material.COAL), 0.2, 0, 3);				//20% spin chance, 0% per slot
 	}
 	
 	@Override
@@ -77,7 +77,6 @@ public class Gamble implements CommandExecutor, Listener {
 					stake = 0;
 				}
 				
-				
 				User user = ess.getUser(player);
 				
 				if(!user.canAfford(new BigDecimal(stake))) {
@@ -92,7 +91,8 @@ public class Gamble implements CommandExecutor, Listener {
 				}
 				
 				player.playSound(player.getLocation(), Sound.BLOCK_BEACON_ACTIVATE, 0.8f, 1);
-				player.playSound(player.getLocation(), Sound.MUSIC_DISC_BLOCKS, 0.4f, 1);
+				
+				player.playSound(player.getLocation(), Sound.MUSIC_DISC_BLOCKS, 0.2f, 1);
 				this.openGambleScreen(player, stake);
 			}	
 			return true;
@@ -112,7 +112,6 @@ public class Gamble implements CommandExecutor, Listener {
 			CasinoPlayer cPlayer = this.getCasinoPlayer(player);
 			if(cPlayer != null) {
 				e.setCancelled(true);
-				
 				this.openGambleScreen(player, cPlayer.getStake());
 			} else {
 				Log.info("Casino Player not found! This should not be possible!");
@@ -125,15 +124,16 @@ public class Gamble implements CommandExecutor, Listener {
 		if(e.getCurrentItem().getType().equals(Material.RED_WOOL)) {
 			e.setCancelled(true);
 			player.closeInventory();
+			player.stopSound(Sound.MUSIC_DISC_BLOCKS);
 			return;
 		}
 		
-		player.stopSound(Sound.MUSIC_DISC_BLOCKS);
 		
 		e.setCancelled(true);
 		player.closeInventory();
 
 	}
+	
 	
 	@EventHandler
 	public void onClose(InventoryCloseEvent e) {
@@ -145,9 +145,19 @@ public class Gamble implements CommandExecutor, Listener {
 		
 
 	}
+	
 
-	private void openGambleScreen(Player player, double moneyAmount) {
-		this.deductMoney(player, moneyAmount);
+	private void openGambleScreen(Player player, double stake) {
+		
+		User user = ess.getUser(player);
+		
+		if(!user.canAfford(new BigDecimal(stake))) {
+			player.sendMessage(ChatColor.BLUE + "Du bist nicht " + ChatColor.GOLD + "reich" +  ChatColor.RESET + " genung um zu zocken!");
+			player.closeInventory();
+			return;
+		}
+		
+		this.deductMoney(player, stake);
 		
 		Inventory inv = Bukkit.createInventory(null, 27, ChatColor.DARK_AQUA + "Feeling lucky?");
 		
@@ -174,7 +184,7 @@ public class Gamble implements CommandExecutor, Listener {
 			final int it = i;
 			
 			
-			plugin.getServer().getScheduler().scheduleSyncDelayedTask(this.plugin, () -> setInv(items, inv, it, player, moneyAmount), time);
+			plugin.getServer().getScheduler().scheduleSyncDelayedTask(this.plugin, () -> setInv(items, inv, it, player, stake), time);
 			
 			preDelay = time;
 		}
@@ -183,7 +193,7 @@ public class Gamble implements CommandExecutor, Listener {
 	    player.openInventory(inv);
 	}
 	
-	private void setInv(ItemStack[] items, Inventory inv, int i, Player player, double moneyAmount) {
+	private void setInv(ItemStack[] items, Inventory inv, int i, Player player, double stake) {
 		Random rnd = new Random();
 	    int rndNum = rnd.nextInt(99) + 1;
 	    
@@ -209,10 +219,10 @@ public class Gamble implements CommandExecutor, Listener {
 	    }
 	    
 	    inv.setItem(i+10, items[i]);
-	    player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_HARP, 1, 1);
+	    player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_HARP, 0.8f, 1);
 	    
 	    if(i == 6) {
-	    	this.calcMoney(items, moneyAmount, player);
+	    	this.calcMoney(items, stake, player);
 	    }
 	}
 	
