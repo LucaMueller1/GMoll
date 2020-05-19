a4a64832a679d3ab0485943eccca096dbd66b463
GameduJS <Sagich@dir.net>
2020-05-19 14:06:32 +0200

    nice shit

--- src/me/YvesLuca/GMoll/events/Welcome.java
+++ src/me/YvesLuca/GMoll/events/Welcome.java
@@ -5,6 +5,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerAdvancementDoneEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerKickEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 import me.YvesLuca.GMoll.Main;
@@ -25,11 +26,17 @@ public class Welcome implements Listener{
 		String player2 = (String) e.getPlayer().getName();
 		player.sendMessage(ChatColor.DARK_AQUA + "Willkommen " + ChatColor.DARK_RED + ChatColor.BOLD + player2 + ChatColor.AQUA + " auf dem Community Server" + "           ----hab wie immer SPAß!----");
 		player.giveExp(1);
+		int healt = (int) player.getHealth();
+		player.setHealth(healt + 1);
+		int food = (int) player.getFoodLevel();
+		player.setFoodLevel(food + 2);
+		
 	}
-//	@EventHandler
-//	public void QuitEvent(PlayerQuitEvent e) {
-//		e.setQuitMessage("");
-//	}
+	@EventHandler
+	public void QuitEvent(PlayerQuitEvent ev) {
+		ev.setQuitMessage("");
+	}
+	
 		
 
 }
