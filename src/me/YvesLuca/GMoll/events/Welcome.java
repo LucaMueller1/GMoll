67b5c1c486eb8f11836265f736797c6dcbd9aec7
GameduJS <Sagich@dir.net>
2020-05-19 11:43:35 +0200

    added Welcome Message

---
+++ src/me/YvesLuca/GMoll/events/Welcome.java
@@ -0,0 +1,27 @@
+package me.YvesLuca.GMoll.events;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+import me.YvesLuca.GMoll.Main;
+import net.md_5.bungee.api.ChatColor;
+
+public class Welcome implements Listener{
+	
+	private Main plugin;
+	
+	public Welcome(Main main) {
+		this.plugin = main;
+	}
+	
+	@EventHandler
+	public void WEvent(PlayerJoinEvent e) {
+		Player player = (Player) e.getPlayer();
+		player.sendMessage(ChatColor.DARK_AQUA + "Willkommen" + ChatColor.AQUA + "auf dem Community Server" + ChatColor.YELLOW + "\nhab wie immer SPAß!");
+		player.giveExp(1);
+	}
+	
+
+}
